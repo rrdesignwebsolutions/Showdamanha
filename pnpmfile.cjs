f9c81cdec3c089c124d943a0acc457cded40ce79
rrdesignwebsolutions <contato@rrdesignwebsolutions.com.br>
2026-07-23 00:26:27 -0300

    nova edic

--- pnpmfile.cjs
+++ pnpmfile.cjs
@@ -1,17 +1,7 @@
 // pnpmfile.cjs
 module.exports = {
   hooks: {
-    readPackage(pkg) {
-      return pkg
-    },
-    filterLog(log) {
-      if (log.code === 'ERR_PNPM_IGNORED_BUILDS') {
-        return false
-      }
-      return true
-    },
     allowBuild(pkg) {
-      // Aprova automaticamente esses pacotes
       if (pkg.name === '@tailwindcss/oxide' || pkg.name === 'esbuild') {
         return true
       }
