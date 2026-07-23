32f7cb89dff8ba49923578e84939c66d7af3d41e
rrdesignwebsolutions <contato@rrdesignwebsolutions.com.br>
2026-07-23 00:30:25 -0300

    nova atualz

--- pnpmfile.cjs
+++ pnpmfile.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   hooks: {
     allowBuild(pkg) {
+      // Aprova automaticamente pacotes que precisam rodar scripts de build
       if (pkg.name === '@tailwindcss/oxide' || pkg.name === 'esbuild') {
         return true
       }
