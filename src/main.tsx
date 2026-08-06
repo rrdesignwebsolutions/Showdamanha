564838d735b7d0545aaab486490ad836324c1ce1
rrdesignwebsolutions <contato@rrdesignwebsolutions.com.br>
2026-08-06 20:22:20 -0300

    favicon atualization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  document.title = "Show da Manhã";
 
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/png";
+favicon.href = "/favicon.png";
+document.head.appendChild(favicon);
+
+createRoot(document.getElementById("root")!).render(<App />);
+
+createRoot(document.getElementById("root")!).render(<App />);
   createRoot(document.getElementById("root")!).render(<App />);
   
